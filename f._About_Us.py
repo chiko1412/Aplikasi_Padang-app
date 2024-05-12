e68e7b0d0fb9aea9cdca20258b45cce93c336d38
chiko1412 <smiqbal1210@gmail.com>
2024-05-12 20:12:59 +0700

    Add files via upload

---
+++ f._About_Us.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('PSB (Padang Sukabumi Bogor)')
+
+st.header('Anggota Kelompok')
+st.subheader('1. Muhammad Fadil Nugraha (2360178)')
+st.subheader('2. Mutia Azatil ismah(2360191)')
+st.subheader('3. Said Muhammad Iqbal (2360253)')
+st.header('Contact Person')
+st.subheader('Fadil (085624826410)')
+st.subheader('Muti (083161993086)')
+st.subheader('Iqbal (085771008112)')
